5de05d43d0724f1cc1bee75191d3c843444da6ea
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-04-28 15:08:12 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,7 @@
 def meal_choice(veg1, veg2, protein = "tofu")
   puts "What a nutritious meal!"
   p "A plate of #{protein} with #{veg1} and #{veg2}."
+end
 
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
